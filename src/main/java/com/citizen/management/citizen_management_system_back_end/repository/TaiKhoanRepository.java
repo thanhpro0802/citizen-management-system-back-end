6ff58751b5d783addd911f51471e0c917656c70d
hinefrv <asd123hien@gmail.com>
2025-11-24 22:46:39 +0700

    Them entities, enums, repositories va securityconfig
    Them entities, enums, repositories va securityconfig.

---
+++ src/main/java/com/citizen/management/citizen_management_system_back_end/repository/TaiKhoanRepository.java
@@ -0,0 +1,7 @@
+package com.citizen.management.citizen_management_system_back_end.repository;
+
+import com.citizen.management.citizen_management_system_back_end.entity.TaiKhoan;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaiKhoanRepository extends JpaRepository<TaiKhoan, String> {
+}
