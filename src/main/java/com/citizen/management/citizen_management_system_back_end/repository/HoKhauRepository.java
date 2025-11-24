494350f7413846c91aed60b7c2ae499fd8cb858b
Nguyễn Tuấn Thành <thanh.nt235833@sis.hust.edu.vn>
2025-11-24 21:58:51 +0700

    Revert "#11 api thong ke co ban"

--- src/main/java/com/citizen/management/citizen_management_system_back_end/repository/HoKhauRepository.java
+++
@@ -1,9 +0,0 @@
-package com.citizen.management.citizen_management_system_back_end.repository;
-
-import com.citizen.management.citizen_management_system_back_end.entity.HoKhau;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HoKhauRepository extends JpaRepository<HoKhau, Long> {
-
-    Long countByPhuong(String phuong);
-}
