13bb08a647df9213de3a3b76f4553c0a14ebf78e
Nguyễn Tuấn Thành <thanh.nt235833@sis.hust.edu.vn>
2025-11-25 14:45:36 +0700

    Revert "Them entities, enums, repositories va securityconfig"

--- src/main/java/com/citizen/management/citizen_management_system_back_end/repository/TaiKhoanRepository.java
+++
@@ -1,7 +0,0 @@
-package com.citizen.management.citizen_management_system_back_end.repository;
-
-import com.citizen.management.citizen_management_system_back_end.entity.TaiKhoan;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaiKhoanRepository extends JpaRepository<TaiKhoan, String> {
-}
