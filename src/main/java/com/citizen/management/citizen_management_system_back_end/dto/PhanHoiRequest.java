8d0f4177ad99a92cd2913ce7ebabfff4924a704a
Nguyễn Tuấn Thành <thanh.nt235833@sis.hust.edu.vn>
2025-12-24 12:31:09 +0700

    Add JsonIgnoreProperties annotations to prevent circular references in entity relationships

--- src/main/java/com/citizen/management/citizen_management_system_back_end/dto/PhanHoiRequest.java
+++
@@ -1,9 +0,0 @@
-package com.citizen.management.citizen_management_system_back_end.dto;
-
-import lombok.Data;
-
-@Data
-public class PhanHoiRequest {
-    //Noi dung tra loi chinh thuc
-    private String noiDungPhanHoi;
-}
--- src/main/java/com/citizen/management/citizen_management_system_back_end/dto/response/JwtResponse.java
+++ src/main/java/com/citizen/management/citizen_management_system_back_end/dto/response/JwtResponse.java
@@ -1,6 +1,14 @@
 package com.citizen.management.citizen_management_system_back_end.dto.response;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import java.util.List;
-@Data @AllArgsConstructor
-public class JwtResponse { private String token; private String id; private String username; private List<String> roles; }
+
+@Data
+@AllArgsConstructor
+public class JwtResponse {
+    private String token;
+    private String id;
+    private String cccd;
+    private List<String> roles;
+}
