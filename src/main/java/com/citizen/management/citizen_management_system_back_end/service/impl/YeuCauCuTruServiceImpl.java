33fbeacdb9df9fdef61c64f97ec5ef0abd639867
hinefrv <asd123hien@gmail.com>
2025-12-28 14:34:58 +0700

    fix
    fxi

--- src/main/java/com/citizen/management/citizen_management_system_back_end/service/impl/YeuCauCuTruServiceImpl.java
+++ src/main/java/com/citizen/management/citizen_management_system_back_end/service/impl/YeuCauCuTruServiceImpl.java
@@ -23,9 +23,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Date;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
